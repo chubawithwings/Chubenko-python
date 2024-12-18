d0cbf89931cbef59a0da1bcfb62bf3527fb41ee3
chubawithwings <alinademidova223@gmail.com>
2024-12-18 19:06:39 +0300

    коммит

--- PZ_6/PZ_6_1.py
+++ PZ_6/PZ_6_1.py
@@ -2,22 +2,19 @@
 #на исходное значение первого четного числа.Если четные числа в списке отсутствуют,
 #то оставить список без изменений.
 
-def spisok(numbers):
-    first_even = None
-    for num in numbers:
-        if num % 2 == 0:
-            first_even = num
-            break
 
-    if first_even is None:
-        return numbers
+A = [1, 2, 5, 6, 8, 7]
+N = len(A)
 
-    modified_numbers = numbers[:]
+first_even = None
+for num in A:
+    if num % 2 == 0:
+        first_even = num
+        break
 
-    for i, num in enumerate(modified_numbers):
-        if num % 2 == 0:
-            modified_numbers[i] += first_even
-    return modified_numbers
-list = [1, 2, 3, 6, 8, 5, 10, 12]
-result = spisok(list)
-print(f"Исходный список: {list}, Измененный список: {result}")
+if first_even is not None:
+    for i in range(N):
+        if A[i] % 2 == 0:
+            A[i] += first_even
+
+print("Измененный список:", A)
