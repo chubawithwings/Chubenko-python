6f9570b38f501110a0468322d7304762c7d4c4f1
chubawithwings <alinademidova223@gmail.com>
2025-06-06 00:54:43 +0300

    update

--- PZ_13/PZ_13_2.py
+++ PZ_13/PZ_13_2.py
@@ -2,16 +2,16 @@
 
 import random
 matrix = [[random.randint(1, 10) for _ in range(4)] for _ in range(4)]
-
 print("Исходная матрица:")
 for a in matrix:
     print(a)
 
-for i in range(4):
-    for j in range(4):
-        if i != j:
-            matrix[i][j] *= 2
-
-print("Полученная матрица:")
-for a in matrix:
-    print(a)
+n = len(matrix)
+result = list(map(
+    lambda i: list(map(
+        lambda j: matrix[i][j] if i == j else matrix[i][j] * 2,
+        range(n))),
+    range(n)))
+print("Получившаяся матрица:")
+for b in result:
+    print(b)
