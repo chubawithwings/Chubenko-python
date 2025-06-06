5031ab2b758adfbeaa95d23c8219705446a95b4a
chubawithwings <alinademidova223@gmail.com>
2025-06-06 04:09:40 +0300

    коммит

---
+++ PZ_14/PZ_14.py
@@ -0,0 +1,12 @@
+# В исходном текстовом файле(radio stations.txt) найти все домены из URL-адресов
+# (например, в URL-apece http://stream.hoster.by:8081/pilotfm/audio/icecast.audio
+# домен выделен полужирным).
+
+import re
+
+with open('radio_stations.txt', 'r', encoding='utf-8') as f1:
+    allfile = f1.read()
+
+res = re.findall(r'[a-z0-9]\w+\.[a-z0-9]\w+\.[a-z0-9]\w+', allfile)
+for i in res:
+    print(i)
